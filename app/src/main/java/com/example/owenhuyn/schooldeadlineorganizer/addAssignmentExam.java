095a141eb552749028f1d201486281f6e131f788
ohuyn <ohuyn@rlsolutions.com>
2015-07-16 16:50:01 -0400

    added date and time button

--- app/src/main/java/com/example/owenhuyn/schooldeadlineorganizer/addAssignmentExam.java
+++ app/src/main/java/com/example/owenhuyn/schooldeadlineorganizer/addAssignmentExam.java
@@ -1,29 +1,65 @@
 package com.example.owenhuyn.schooldeadlineorganizer;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class addAssignmentExam extends AppCompatActivity {
     private Toolbar toolbar;
-    private Button date_button;
+    public static Button date_button;
+    public static Button time_button;
+    Context mContext;
+
+    private static final int DATE_DIALOG_ID = 1;
+    private int year;
+    private int month;
+    private int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_submission_dialog);
+        mContext = this;
 
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         //getSupportActionBar().setHomeAsUpIndicator(R.string.SAVE);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        setTitle("Add new event");
+        setTitle("Add new assignment");
 
         date_button = (Button) findViewById(R.id.date_button);
+        date_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+        time_button = (Button) findViewById(R.id.time_button);
+        time_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
     }
 
     @Override
@@ -47,5 +83,50 @@ public class addAssignmentExam extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            //return new DatePickerDialog(getActivity(), (EditSessionActivity)getActivity(), year, month, day);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+
+            date_button.setText(String.valueOf(day) + "/"
+                    + String.valueOf(month + 1) + "/" + String.valueOf(year));
+            // set selected date into datepicker also
+
+
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute, true);
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            time_button.setText(String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+        }
     }
 }
